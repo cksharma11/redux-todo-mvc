084bda0207739777adf1e1e7e0c1b3157b5f0f23
Chandan Kumar <cksharma122@gmail.com>
2020-05-25 20:27:59 +0530

    Updates

---
+++ precache-manifest.ad7d9e1ba75b4f97824860285941929b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b26c1c043e95e02934667d280fcf413",
+    "url": "/redux-todo-mvc/index.html"
+  },
+  {
+    "revision": "51223d9ad31b47381bec",
+    "url": "/redux-todo-mvc/static/css/2.f124e354.chunk.css"
+  },
+  {
+    "revision": "19a89751ef2d4b837913",
+    "url": "/redux-todo-mvc/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "51223d9ad31b47381bec",
+    "url": "/redux-todo-mvc/static/js/2.e338179e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/redux-todo-mvc/static/js/2.e338179e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19a89751ef2d4b837913",
+    "url": "/redux-todo-mvc/static/js/main.aed0c222.chunk.js"
+  },
+  {
+    "revision": "4feae62869064293dafc",
+    "url": "/redux-todo-mvc/static/js/runtime-main.a9691ddb.js"
+  }
+]);
